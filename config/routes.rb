9dfe6dcb74287154648cf09dd6e01aed82b0365b
Sok444 <sok_88@mail.ru>
2017-11-07 21:46:57 +0300

    Change /contacts/new -> /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'home#index'
   
-  resource :contacts, only: [:new, :create]
+  #resource :contacts, only: [:new, :create]
+
+  #1-ый способ чтобы url http://localhost:3000/contacts обрабаотывал запрос GET
+  #прописать маршрут вручную
+  #get 'contacts' => 'contacts#new'
+  #resource :contacts, only: [:create]
+
+  #2-ой способ экшен new теперь доступен по базавому пути /contacts/ и метод GET
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
 
   resources :articles
 end
