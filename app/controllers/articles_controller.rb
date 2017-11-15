6c3709ba170639776021392c188010d61c169f0a
Sok44 <sok_88@mail.ru>
2017-11-15 10:29:42 +0300

    Authenticate user for new/create actions only

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   #Настройки для devise
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create]
   
   def index
     @articles = Article.all
