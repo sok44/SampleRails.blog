32a0dfb1e15686925ab6525d9a850215e03b9ce3
Sok44 <sok_88@mail.ru>
2017-11-25 14:49:30 +0300

    Update article_spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 describe Article do 
  
   #Для удобства можно разделять тесты по логическому смыслу
+  
   describe "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
@@ -11,6 +12,17 @@ describe Article do
 
   describe "associations" do
     it { should have_many :comments }
+  end
+
+  #Тесты для метода subject
+  describe "#subject" do
+    it "returns the article title" do
+      #создаем объект средствами гема factory girl, который заменяет ActiveRecord 
+      article = create(:article, title: 'Lorem Ipsum')
+      
+      #assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
   end
   
 end
