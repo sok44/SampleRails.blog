909e23ff015779ee20edfa844d0ba6da0afcdf3f
Sok444 <sok_88@mail.ru>
2017-11-26 12:46:22 +0300

    Update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,5 +1,4 @@
 #С какой то версии Rails надо указывать rails_helper,а не spec_helpers, т.к. о содержит ссылку на spec_helpers
-
 require "rails_helper"
 
 #Тестируем создание контакта
@@ -8,7 +7,7 @@ feature "Contact Creation" do
   scenario "allows acces to contacts page" do
     #Заходим на указанную страницу
     visit '/contacts'
-    #Ищем на этой странице в содержимом указанный текст
-    expect(page).to have_content 'Contact us'
+    #Ищем на этой странице в содержимом указанный текст, текст указан с помощью механизма I18n
+    expect(page).to have_content  I18n.t 'contacts.contact_us'
   end  
 end
